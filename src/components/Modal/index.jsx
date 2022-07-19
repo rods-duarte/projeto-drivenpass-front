45e9c13ccea504e2839ad1e7d67c8bfd2591f7b8
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-07-18 23:51:34 -0300

    Feat: add button function

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,12 +1,23 @@
 import { ModalContainer, Content } from './style';
 
-export default function Modal({ title, message, setOpenModal }) {
+export default function Modal({
+  title,
+  message,
+  setOpenModal,
+  doFunction = null,
+}) {
   return (
     <ModalContainer>
       <Content>
         <h1>{title}</h1>
         <p>{message}</p>
-        <button type="button" onClick={() => setOpenModal(false)}>
+        <button
+          type="button"
+          onClick={() => {
+            setOpenModal(false);
+            if (doFunction) doFunction();
+          }}
+        >
           Ok
         </button>
       </Content>
