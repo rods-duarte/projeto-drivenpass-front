0a285481c80f3e769e7efa8165ef3e73cdafb493
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-07-18 21:20:44 -0300

    Feat: add logout function

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,33 @@
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { HeaderContainer } from './style';
 import logo from '../../assets/images/Logo.svg';
 import logout from '../../assets/images/exit.svg';
+import { TokenContext } from '../../contexts/tokenContext';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const { token, setToken } = useContext(TokenContext);
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/');
+    }
+  }, [token]);
+
   return (
     <HeaderContainer>
       <div>
         <img src={logo} alt="Logo" />
         <h1>DrivenPass</h1>
       </div>
-      <img src={logout} alt="Logout" />
+      <img
+        src={logout}
+        alt="Logout"
+        onClick={() => {
+          setToken(null);
+        }}
+      />
     </HeaderContainer>
   );
 }
